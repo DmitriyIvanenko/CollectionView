80f0b95c0c2efc22ff4f49e26b592d53a4719905
Dmytro Ivanenko <ivanenko2010@gmail.com>
2023-02-14 14:14:43 +0300

    Initial Commit

---
+++ CollectionView2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionView2
+//
+//  Created by Dmytro Ivanenko on 14.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
